2273e2f7df31d98f67dac38454bc9654eaeb85c0
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-08 16:01:24 -0500

    star wars: added for loop to then function

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 $(() => {
 //onload
 
-const promise = $.ajax({
-    url:'https://swapi.co/api/people/'
+  //use ajax to access star wars api
+  const promise = $.ajax({
+      url:"https://swapi.co/api/species/"
+  })  //then do something with information from api
+    .then(
+      (data) => {
+        console.log(data);
+        //use a for loop to grab 10 species from api
+        for (var i = 0; i < 10 ; i++) {
+          console.log(data.results[i]);
 
-});
-
-
-promise.then(
-  (data) => {
-    console.log(data);
-  }
-)
+        }
+      }
+    )
 
 
 
