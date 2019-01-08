e39d8a32eb07f71987d02bd72f0c1f4ae3f23228
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-08 13:54:02 -0500

    star wars: connected to api with ajax

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 $(() => {
 //onload
 
+const promise = $.ajax({
+    url:'https://swapi.co/api/people/'
+
+});
+
+
+promise.then(
+  (data) => {
+    console.log(data);
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
