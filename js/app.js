2d2c2dfdad8a2d54539f8dd6b5a0488a6a7856ac
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-09 10:32:51 -0500

    star wars: used for loop to put character names into divs

--- js/app.js
+++ js/app.js
@@ -3,18 +3,21 @@ $(() => {
 
   //use ajax to access star wars api
   const promise = $.ajax({
-      url:"https://swapi.co/api/species/"
-  })  //then do something with information from api
-    .then(
-      (data) => {
-        console.log(data);
-        //use a for loop to grab 10 species from api
-        for (var i = 0; i < 10git  ; i++) {
-          console.log(data.results[i]);
+      url:"https://swapi.co/api/people/?page=2"
+  }).then(
+    (data) => {
+      console.log(data.results);
+      // use a for loop to grab 10 species from api
+      for (var i = 0; i < 10; i++) {
+        const $carousel_image = $('<div>').addClass('carousel_card')
+        $($carousel_image).text(data.results[i].name)
+        $('#carousel_container').append($carousel_image)
 
-        }
+
       }
-    )
+    }
+  )
+
 
 
 
