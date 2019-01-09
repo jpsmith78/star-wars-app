9201e9d3133c4377331d0a02a891fc8bb1e36145
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-09 12:56:44 -0500

    player card event listener and event handler created

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 $(() => {
 //onload
 
-
-
+  //==================================================
+  //<<<<<<<<<<<<<<<<<<PROMISE>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //===================================================
   //use ajax to access star wars api
-  const promise = $.ajax({
+  $.ajax({
       url:"https://swapi.co/api/people/?page=2"
   }).then(
     (data) => {
-      console.log(data.results);
+      // console.log(data.results);
       // use a for loop to grab 10 species from api
       for (var i = 0; i < 10; i++) {
         //create carousel card div
@@ -18,13 +19,17 @@ $(() => {
         //append carousel card to carousel images div
         $('#carousel_divs').append($carousel_card)
       }
+      //==================================================
+      //<<<<<<<<<<<<<<<<<CAROUSEL BUTTON>>>>>>>>>>>>>>>>>>>
+      //==================================================
+
       //create current div counter variable for carousel
       let currentDivIndex = 0;
       //count how many divs are in the carousel
       const numberOfDivs = $('#carousel_divs').children().length-1;
-      console.log(numberOfDivs);
+      // console.log(numberOfDivs);
 
-      //next button
+      //NEXT BUTTON
       $('#next_button').on('click',() => {
         //hide current div
         $('#carousel_divs').children().eq(currentDivIndex).css('display','none')
@@ -42,7 +47,7 @@ $(() => {
       })
       $('#carousel_divs').children().eq(currentDivIndex).css('display','block')
 
-      //back_button
+      //BACK BUTTON
       $('#back_button').on('click',() => {
         //hide current div
         $('#carousel_divs').children().eq(currentDivIndex).css('display','none')
@@ -58,10 +63,14 @@ $(() => {
           $('#carousel_divs').children().eq(currentDivIndex).css('display','block')
         }
       })
+      $('#player_card').on('click',(event) => {
+        if($(event.target).text()==''){
+          $(event.target).append($('#carousel_divs').children().eq(currentDivIndex))
+        }
+
+      })
     }
   )
-
-
 
 
 
