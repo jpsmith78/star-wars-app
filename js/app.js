a8791a85b33e693817127b8170e83eb4805195bc
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-01-09 11:03:00 -0500

    star wars: created current div counter for carousel

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(() => {
 //onload
 
+
+
   //use ajax to access star wars api
   const promise = $.ajax({
       url:"https://swapi.co/api/people/?page=2"
@@ -9,12 +11,19 @@ $(() => {
       console.log(data.results);
       // use a for loop to grab 10 species from api
       for (var i = 0; i < 10; i++) {
-        const $carousel_image = $('<div>').addClass('carousel_card')
-        $($carousel_image).text(data.results[i].name)
-        $('#carousel_container').append($carousel_image)
-
-
+        //create carousel card div
+        const $carousel_card = $('<div>').addClass('carousel_card')
+        //add people names to carousel cards
+        $($carousel_card).text(data.results[i].name)
+        //append carousel card to carousel images div
+        $('#carousel_divs').append($carousel_card)
       }
+      //create current div counter variable for carousel
+      let currentImageIndex = 0;
+      //count how many divs are in the carousel
+      const numberOfDivs = $('#carousel_divs').children().length-1;
+      console.log(numberOfDivs);
+
     }
   )
 
